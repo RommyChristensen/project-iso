43f0e44397d0ad3a8ad1ab65b6767654f042a4ba
Ansell Benedy <ansell08.es@gmail.com>
2022-01-06 15:13:45 +0700

    insert chat --

--- src/components/UserChat.js
+++ src/components/UserChat.js
@@ -1,4 +1,6 @@
+import * as React from 'react';
 import { fire } from "../config/firebase";
+import { arrayUnion, arrayRemove } from "firebase/firestore";
 import { query, collection, where, getDocs, addDoc, orWhere, orderBy, startAt, endAt, updateDoc, doc, getDoc, setDoc } from 'firebase/firestore/lite';
 import { onSnapshot } from 'firebase/firestore';
 import { useContext, useState } from "react";
@@ -17,16 +19,44 @@ const rightStyle = {
     right:10
 }
 
+
 const UserChat = () => {
     const { userActive, setUser, room, setRoom, activeRoom } = useContext(UserContext);
     const [ text, setText ] = useState('');
+    const [selectContact, setSelectContact] = React.useState({});
     
     const getMessages = () => {
         if(activeRoom){
             const doc = fire.collection('room').doc(activeRoom.id);
         }
     }
+    async function  sendChat(event){
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        // // eslint-disable-next-line no-console
+        let chats = data.get('message');
+        // alert(activeRoom.id)
 
+        let newChat=
+        {
+            "from":userActive.username,
+            "message":chats,
+            "read":false,
+            "sent_time":new Date().toLocaleString()
+        }
+
+        console.log(newChat)
+        const db = collection(fire, 'room');
+        // const selectedRoom = fire.collection('room').doc(activeRoom.id);
+        // console.log(selectedRoom)
+        // var docChat = fire.collection('room').doc(activeRoom.id);
+        const docChat = doc(db, "room", activeRoom.id);
+        console.log(docChat)
+        await updateDoc(docChat, {
+            chats: arrayUnion(newChat)
+        });
+        
+    };
     const generateMessages = () => {
         console.log("chats", activeRoom.chats);
         const chats = activeRoom.chats;
@@ -81,16 +111,14 @@ const UserChat = () => {
                 </CardContent>
                 </Card> */}
             </Grid>
-            <Grid item style={{width:"80%",position:"absolute",bottom:'10px',marginLeft:'10px', marginRight:'10px'}}>
+            <Grid item component="form" onSubmit={sendChat} style={{width:"70%",position:"absolute",bottom:'10px',marginLeft:'10px', marginRight:'10px'}}>
                 {/* <Grid item style={{flex:3, width:"60%"}} > */}
-                    <TextField onChange={(e) => setText(e.target.value)} style={{width:"90%",backgroundColor:"white"}} hiddenLabel fullWidth id="standard-basic" placeholder='Enter A Message' variant="standard"  />
+                    <TextField name="message" onChange={(e) => setText(e.target.value)} style={{width:"90%",backgroundColor:"white"}} hiddenLabel fullWidth id="standard-basic" placeholder='Enter A Message' variant="standard"  />
 
                 {/* </Grid> */}
                 {/* <Grid item style={{flex:1,width:"40%"}}  > */}
                 <Button style={{width:"10%"}}
-                    onClick={() => {
-                    alert('clicked');
-                    }}
+                    type='submit'
                 >
                     SEND
                 </Button>
